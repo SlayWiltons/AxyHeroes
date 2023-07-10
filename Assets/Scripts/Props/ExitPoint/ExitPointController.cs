c7dffebec5d89450754a98f80728e5806083492d
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-07-10 21:38:15 +0300

    Added exitPoints for ActionWithHold trigger

---
+++ Assets/Scripts/Props/ExitPoint/ExitPointController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ExitPointController : BasePropsController
+{
+    [SerializeField] private GameObject _textPlate;
+
+    private InputAction TestInputAction;
+    private PlayerInput _playerInput;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        _playerInput = collider.gameObject.GetComponent<PlayerInput>();
+        TestInputAction = _playerInput.actions.FindAction("ActionWithHold");
+        TestInputAction.performed += Action;
+        ShowPlate(_textPlate);
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        TestInputAction.performed -= Action;
+        HidePlate(_textPlate);
+    }
+
+    public override void Action(InputAction.CallbackContext context)
+    {
+        HidePlate(_textPlate);
+        Debug.Log("Exit");
+    }
+}
