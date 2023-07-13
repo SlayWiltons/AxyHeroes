19a597d6140821c3e6b4c2688ed0b4cde95a8d63
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-07-13 22:17:39 +0300

    Created base stats list

--- Assets/Scripts/Characters/Stats_Base.cs
+++ Assets/Scripts/Characters/Stats_Base.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New_Character", menuName = "New character", order = 1)]
-public class Character_Base : ScriptableObject
+[CreateAssetMenu(fileName = "Stats_list", menuName = "Stats list", order = 1)]
+public class Stats_Base : ScriptableObject
 {
     [SerializeField] private int _STR;
     [SerializeField] private int _AGI;
