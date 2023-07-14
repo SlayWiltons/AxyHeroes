d18a7a6818d878b42e7a3e8eff48bf135c5afac1
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-07-14 22:18:57 +0300

    Preparing character class

---
+++ Assets/Scripts/Characters/Character_Base.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Base : MonoBehaviour
+{
+    public string Name;
+    public Stats_Base BaseStats;
+}
--- Assets/Scripts/Characters/Stats_Base.cs
+++ Assets/Scripts/Characters/Stats_Base.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Stats_list", menuName = "Stats list", order = 1)]
 public class Stats_Base : ScriptableObject
 {
-    [SerializeField] private int _STR;
-    [SerializeField] private int _AGI;
-    [SerializeField] private int _INT;
-    [SerializeField] private int _END;
-    [SerializeField] private int _WIS;
-    [SerializeField] private int _PER;
-    [SerializeField] private int _LUC;
+    public int _STR;
+    public int _AGI;
+    public int _INT;
+    public int _END;
+    public int _WIS;
+    public int _PER;
+    public int _LUC;
 }
