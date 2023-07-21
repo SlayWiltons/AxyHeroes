025551ed0cbea68ab91e697c4992437df46931fb
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-07-21 22:21:29 +0300

    Prepearations to character's creating

--- Assets/Scripts/Characters/Character_Base.cs
+++ Assets/Scripts/Characters/Character_Base.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Character_Base : MonoBehaviour
+[CreateAssetMenu(fileName = "Character_Base", menuName = "Character Base", order = 2)]
+public class Character_Base : ScriptableObject
 {
     public string Name;
+    public RawImage Avatar;
+
+    public enum Alignment
+    {
+        Good,
+        Neutral,
+        Evil
+    }
+
+    public Alignment CharAlignment;
+
     public Stats_Base BaseStats;
 }
