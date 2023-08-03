d4d92d86bd2eef66fc1e7cab2c7f422a31992693
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-08-03 20:37:31 +0300

    Improved Stats_controller

--- Assets/Scripts/Stats_Controller.cs
+++ Assets/Scripts/Stats_Controller.cs
@@ -1,7 +1,40 @@
+using TMPro;
 using UnityEngine;
 
 public class Stats_Controller : MonoBehaviour
 {
     [SerializeField] private Classes_Container _classesContainer;
+    [SerializeField] private TMP_Text _className;
+    [SerializeField] private TMP_Text _classSTR;
+    [SerializeField] private TMP_Text _classAGI;
+    [SerializeField] private TMP_Text _classINT;
+    [SerializeField] private TMP_Text _classEND;
+    [SerializeField] private TMP_Text _classWIS;
+    [SerializeField] private TMP_Text _classPER;
+    [SerializeField] private TMP_Text _classLUC;
+    private int _classesCount;
+
+    private void Start()
+    {
+        _classesCount = _classesContainer.ClassesCount();
+        SetClassParams(0);
+        Debug.Log(_classesCount);
+    }
+
+    private void SetClassParams(int i)
+    {
+        if (_classesCount != 0)
+        {
+            _className.text = _classesContainer.CharacterClass(i).Name;
+            _classSTR.text = _classesContainer.CharacterClass(i).Str.ToString();
+            _classAGI.text = _classesContainer.CharacterClass(i).Agi.ToString();
+            _classINT.text = _classesContainer.CharacterClass(i).Int.ToString();
+            _classEND.text = _classesContainer.CharacterClass(i).End.ToString();
+            _classWIS.text = _classesContainer.CharacterClass(i).Wis.ToString();
+            _classPER.text = _classesContainer.CharacterClass(i).Per.ToString();
+            _classLUC.text = _classesContainer.CharacterClass(i).Luc.ToString();
+        }
+        else return;
+    }
 
 }
