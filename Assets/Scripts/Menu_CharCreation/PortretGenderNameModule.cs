3e60b2b663710a03d86a326e5fe88902ada9a15c
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-08-22 20:18:08 +0300

    Some changes in Module_1

--- Assets/Scripts/Menu_CharCreation/PortretGenderNameModule.cs
+++ Assets/Scripts/Menu_CharCreation/PortretGenderNameModule.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PortretGenderNameModule : MenuModule
 {
     [SerializeField] private GameObject classChooseModule;
+    [SerializeField] private Image _portret;
+    [SerializeField] private List<Texture> _malePortrets;
+    [SerializeField] private List<Sprite> _femalePortrets;
+
+    public void OnEnable()
+    {
+        _portret.sprite = _femalePortrets[0];
+    }
 
     public override void NextModule()
     {
