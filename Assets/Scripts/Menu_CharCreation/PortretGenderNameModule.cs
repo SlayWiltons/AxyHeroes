03d24738a90c2a75fd6d329e6e5b7d06f38a79ab
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-08-23 19:49:05 +0300

    Added "change portrets" functions

--- Assets/Scripts/Menu_CharCreation/PortretGenderNameModule.cs
+++ Assets/Scripts/Menu_CharCreation/PortretGenderNameModule.cs
@@ -8,15 +8,50 @@ public class PortretGenderNameModule : MenuModule
     [SerializeField] private Image _portret;
     [SerializeField] private List<Texture> _malePortrets;
     [SerializeField] private List<Sprite> _femalePortrets;
+    private int _portretId = 0;
+
+    public void ChooseMalePortrets()
+    {
+        
+    }
+
+    public void ChooseFemalePortrets()
+    {
+
+    }
 
     public void OnEnable()
     {
-        _portret.sprite = _femalePortrets[0];
+        _portret.sprite = _femalePortrets[_portretId];
+    }
+
+    public void NextPortret()
+    {
+        ChangePortret(1);
+    }
+
+    public void PrevPortret()
+    {
+        ChangePortret(-1);
     }
 
     public override void NextModule()
     {
         classChooseModule.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    private void ChangePortret(int delta)
+    {
+        _portretId += delta;
+        if (_femalePortrets.Count - 1 < _portretId)
+        {
+            _portretId = 0;
+        }
+        else if (_portretId < 0)
+        {
+            _portretId = _femalePortrets.Count - 1;
+        }
+        _portret.sprite = _femalePortrets[_portretId];
     }
 }
