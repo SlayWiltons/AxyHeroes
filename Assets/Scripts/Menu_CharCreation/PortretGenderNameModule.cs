0ee342ff67aa8372dcbcc54485ff8c3c4379aa90
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-08-24 19:51:23 +0300

    Some updates

--- Assets/Scripts/Menu_CharCreation/PortretGenderNameModule.cs
+++ Assets/Scripts/Menu_CharCreation/PortretGenderNameModule.cs
@@ -1,23 +1,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
+using Image = UnityEngine.UI.Image;
 
 public class PortretGenderNameModule : MenuModule
 {
     [SerializeField] private GameObject classChooseModule;
     [SerializeField] private Image _portret;
+    [SerializeField] private Button _maleButton;
+    [SerializeField] private Button _femaleButton;
     [SerializeField] private List<Texture> _malePortrets;
     [SerializeField] private List<Sprite> _femalePortrets;
+    private Color _selectedColor = Color.green;
+    private Color _normalColor = Color.white;
     private int _portretId = 0;
 
     public void ChooseMalePortrets()
     {
-        
+        ChangeColor(_maleButton, _selectedColor);
+        ChangeColor(_femaleButton, _normalColor);
     }
 
     public void ChooseFemalePortrets()
     {
-
+        ChangeColor(_femaleButton, _selectedColor);
+        ChangeColor(_maleButton, _normalColor);
     }
 
     public void OnEnable()
@@ -39,6 +47,15 @@ public class PortretGenderNameModule : MenuModule
     {
         classChooseModule.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    private void ChangeColor(Button button, Color color)
+    {
+        ColorBlock colors = button.GetComponent<Button>().colors;
+        colors.highlightedColor = color;
+        colors.normalColor = color;
+        colors.pressedColor = color;
+        button.GetComponent<Button>().colors = colors;
     }
 
     private void ChangePortret(int delta)
