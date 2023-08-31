64419ab84b8716c4be8bf5fb3d2bd76720350235
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-08-31 19:43:05 +0300

    Started scripting Module_2

--- Assets/Scripts/Menu_CharCreation/ClassStatsModule.cs
+++ Assets/Scripts/Menu_CharCreation/ClassStatsModule.cs
@@ -1,8 +1,22 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ClassStatsModule : MenuModule
 {
+    [SerializeField] private Classes_Container _classesContainer;
 
+    [SerializeField] private TMP_Text _freePointsText;
+    [SerializeField] private TMP_Text _className;
+
+    [SerializeField] private TMP_Text _classSTR;
+    [SerializeField] private TMP_Text _classAGI;
+    [SerializeField] private TMP_Text _classINT;
+    [SerializeField] private TMP_Text _classEND;
+    [SerializeField] private TMP_Text _classWIS;
+    [SerializeField] private TMP_Text _classPER;
+    [SerializeField] private TMP_Text _classLUC;
+
+    [SerializeField] private List<GameObject> _plusButtons;
+    [SerializeField] private List<GameObject> _minusButtons;
 }
