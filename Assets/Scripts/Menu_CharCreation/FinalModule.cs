1b63377f76658b372fe9b21d8270431301a8a1da
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-09-07 19:46:55 +0300

    Preparing Module_3

--- Assets/Scripts/Menu_CharCreation/FinalModule.cs
+++ Assets/Scripts/Menu_CharCreation/FinalModule.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class FinalModule : MonoBehaviour
+public class FinalModule : MenuModule
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private MenuModule _modulePortret;
+    [SerializeField] private MenuModule _moduleClass;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private TMP_Text _name;
+
+    [SerializeField] private TMP_Text _className;
+
+    [SerializeField] private Image _portret;
+
+    [SerializeField] private TMP_Text _classSTR;
+    [SerializeField] private TMP_Text _classAGI;
+    [SerializeField] private TMP_Text _classINT;
+    [SerializeField] private TMP_Text _classEND;
+    [SerializeField] private TMP_Text _classWIS;
+    [SerializeField] private TMP_Text _classPER;
+    [SerializeField] private TMP_Text _classLUC;
 }
