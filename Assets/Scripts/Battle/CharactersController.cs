0e70f59069c2ac9b06450a4267bf15d9f426e8d1
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-09-09 18:56:40 +0300

    BattleScene: some SO testings

--- Assets/Scripts/Battle/CharactersController.cs
+++ Assets/Scripts/Battle/CharactersController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class CharactersController : MonoBehaviour
 {
+    [SerializeField] private Character_Template _characterTemplateForTest;
     [SerializeField] private List<Character_Template> _characters;
+    private Character_Template charactur;
 
     private void Start()
     {
+        for (int i = 0; i < 4; i++)
+        {
+            //_characters.Add(charactur = new Character_Template());
+            _characters.Add(charactur = ScriptableObject.CreateInstance<Character_Template>());
+        }
         for (int i = 0; i < _characters.Count; i++)
         {
             _characters[i].SetSTR(Random.Range(1, 10));
