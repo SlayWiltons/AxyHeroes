b4720919e8795adae64f5bdf0e33c908e62ed564
Oleg Dvoretskiy <slaywiltons@gmail.com>
2023-09-09 11:37:59 +0300

    Started BattleScene

---
+++ Assets/Scripts/Battle/Character_Template.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Character Template", menuName = "Character Template", order = 2)]
+public class Character_Template : ScriptableObject
+{
+    [SerializeField] private bool _isCharacter;
+    [SerializeField] private string _name;
+    [SerializeField] private string _className;
+    [SerializeField] private int _str;
+    [SerializeField] private int _agi;
+    [SerializeField] private int _int;
+    [SerializeField] private int _end;
+    [SerializeField] private int _wis;
+    [SerializeField] private int _per;
+    [SerializeField] private int _luc;
+
+    public int GetSTR()
+    {
+        return _str;
+    }
+
+    public void SetSTR(int value)
+    {
+        _str = value;
+    }
+}
---
+++ Assets/Scripts/Battle/CharactersController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersController : MonoBehaviour
+{
+    [SerializeField] private List<Character_Template> _characters;
+
+    private void Start()
+    {
+        for (int i = 0; i < _characters.Count; i++)
+        {
+            _characters[i].SetSTR(Random.Range(1, 10));
+        }
+
+        for (int i = 0; i < _characters.Count; i++)
+        {
+            Debug.Log(_characters[i].GetSTR());
+        }
+    }
+}
